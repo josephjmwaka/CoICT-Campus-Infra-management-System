71b09738a17151c13015a88a5ddd01873f5111eb
Marcel-mosha <marcellmosha84@gmail.com>
2025-03-26 14:51:48 +0300

    make use of postgres database

--- infrastructure_management/settings.py
+++ infrastructure_management/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,10 +94,15 @@ WSGI_APPLICATION = 'infrastructure_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('SECRET_KEY'),
+        'HOST': 'localhost',  
+        'PORT': os.getenv('PORT'),  
     }
 }
+
 
 
 # Password validation
@@ -142,4 +150,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
